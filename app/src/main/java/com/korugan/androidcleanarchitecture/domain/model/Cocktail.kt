d18b28e19bbf294ea399ad64452ef086be3b722c
Korugan32 <tahacansenel@gmail.com>
2024-11-08 14:06:22 +0300

    Create Cocktail

---
+++ app/src/main/java/com/korugan/androidcleanarchitecture/domain/model/Cocktail.kt
@@ -0,0 +1,10 @@
+package com.korugan.androidcleanarchitecture.domain.model
+
+data class Cocktail(
+    val idDrink: String,
+    val strAlcoholic: String,
+    val strCategory: String,
+    val strDrink: String,
+    val strDrinkThumb: String,
+    val strInstructions: String,
+)
