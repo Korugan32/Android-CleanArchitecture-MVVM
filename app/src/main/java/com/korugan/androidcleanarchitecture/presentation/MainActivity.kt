38149d984c96e0b61d0bbf7f528caa84a4d2720c
Korugan32 <tahacansenel@gmail.com>
2024-11-08 20:18:07 +0300

    Update MainActivity

--- app/src/main/java/com/korugan/androidcleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/korugan/androidcleanarchitecture/presentation/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.korugan.androidcleanarchitecture.presentation.common.navigation.Navigation
+import com.korugan.androidcleanarchitecture.presentation.home.HomeScreen
 import com.korugan.androidcleanarchitecture.presentation.ui.theme.AndroidCleanArchitectureTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AndroidCleanArchitectureTheme {
-
+               Navigation()
             }
         }
     }
