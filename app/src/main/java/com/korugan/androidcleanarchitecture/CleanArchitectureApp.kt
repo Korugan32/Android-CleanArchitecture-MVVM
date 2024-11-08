eb253f9e4ee70bf46efa8413e6a052c6f17b36eb
Korugan32 <tahacansenel@gmail.com>
2024-11-08 14:08:14 +0300

    Create CleanArchitectureApp(HiltAndroidApp)

---
+++ app/src/main/java/com/korugan/androidcleanarchitecture/CleanArchitectureApp.kt
@@ -0,0 +1,7 @@
+package com.korugan.androidcleanarchitecture
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CleanArchitectureApp : Application()
